e61922daddb6336c1cce7fc54c61d3c8251d00a6
Alien <lmllewellyn@gmail.com>
2024-09-17 23:00:42 -0400

    added logic to database.js for put and get operations

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,38 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const editorDB = await openDB('jate', 1);
+
+  const tx = editorDB.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.put({ jate: content});
+
+  const result = await request;
+
+  console.log('Data saved to the database', result);
+  
+  console.error('putDb not implemented');
+} 
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const editorDB = await openDB('jate', 1);
+
+  const tx = editorDB.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.get(id);
+
+  const result = await request;
+
+  console.log('result.value', result);
+
+  console.error('getDb not implemented');
+} 
 
 initdb();
